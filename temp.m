6227790e19c58f2fd795fa58c83f4dbcad00c35e
Jin Woong Kim <johnjinkim1@gmail.com>
2017-03-10 07:15:06 -0800

    Delete temp.m

--- temp.m
+++
@@ -1,9 +0,0 @@
-//
-//  temp.m
-//  Rendezvous
-//
-//  Created by John Jin Woong Kim on 8/26/15.
-//  Copyright (c) 2015 John Jin Woong Kim. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
